ca74580b62816e8de55dfbc8e3b494cb43f85ad0
mabuyahya <mabuyahya@student.42amman.com>
2025-08-23 20:00:55 +0300

    starting with cpp00 and finishing ex00 and starting with ex01

---
+++ cpp00/ex00/megaphone.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+
+int main(int argc, char **argv)
+{
+	int i;
+	int j;
+
+	i = 1;
+	j = 0;
+	if (argc > 1)
+	{
+		while (i < argc) 
+		{
+			j = 0;
+			while (argv[i][j])
+			{
+				std::cout << (char) std::toupper(argv[i][j]);
+				j++;
+			}
+			i++;
+		}
+		std::cout << std::endl;
+	}
+	else
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *\n";
+	return (0);
+}
---
+++ cpp00/ex01/Contact.hpp
@@ -0,0 +1,12 @@
+#include <string>
+
+class Contact{
+    private:
+        std::string first_name;
+        std::string last_name;
+        std::string nick_name;
+        std::string darkest_secret;
+        std::string phone_number;
+
+
+};
